06c89b636afb1989ecab7d48866d4cb4539ab559
tmampa <tmampa94@gmail.com>
2022-02-11 16:45:55 +0200

    Update project links

--- js/insertions.js
+++ js/insertions.js
@@ -1,108 +1,108 @@
 const portfolioContainer = document.getElementById('Portfolio');
 const modalsContainer = document.getElementsByClassName('modals_container')[0];
 const projects = [
-  {
-    id: 1,
-    title: 'Book store',
-    details: {
-      client: 'Learning Project',
-      role: 'Full Stack Dev',
-      year: '2022',
-    },
-    shortDescription: `The Bookstore is a website that allows you to display a list of books,
+	{
+		id: 1,
+		title: "Book store",
+		details: {
+			client: "Learning Project",
+			role: "Full Stack Dev",
+			year: "2022",
+		},
+		shortDescription: `The Bookstore is a website that allows you to display a list of books,
     add a book, remove a selected book..`,
-    description: `The Bookstore is a website that allows you to display a list of books,
+		description: `The Bookstore is a website that allows you to display a list of books,
     add a book, remove a selected book. Categories page is coming soon`,
-    image: {
-      imageUrl: './img/bookstore.gif',
-      imageAlt: 'preview of BookStore project',
-    },
-    technologies: ['HTML5', 'CSS3', 'JavaScript', 'React', 'Redux'],
-    liveUrl: 'https://m0rrighan.github.io/bookstore-react-redux/',
-    soureUrl: 'https://github.com/M0rrighan/bookstore-react-redux',
-  },
-  {
-    id: 2,
-    title: 'Math Magicians',
-    details: {
-      client: 'Learning Project',
-      role: 'Full Stack Dev',
-      year: '2022',
-    },
-    shortDescription: `A Single Page App (SPA) that allows users to make simple calculations
+		image: {
+			imageUrl: "./img/bookstore.gif",
+			imageAlt: "preview of BookStore project",
+		},
+		technologies: ["HTML5", "CSS3", "JavaScript", "React", "Redux"],
+		liveUrl: "https://bookstorecms-react.netlify.app/",
+		soureUrl: "https://github.com/tmampa/bookstore-cms",
+	},
+	{
+		id: 2,
+		title: "Math Magicians",
+		details: {
+			client: "Learning Project",
+			role: "Full Stack Dev",
+			year: "2022",
+		},
+		shortDescription: `A Single Page App (SPA) that allows users to make simple calculations
     and read a random math-related quote.`,
-    description: `Math magicians" is a website for all fans of mathematics. It is a Single Page App (SPA) that allows users to make simple calculations and
+		description: `Math magicians" is a website for all fans of mathematics. It is a Single Page App (SPA) that allows users to make simple calculations and
     read a random math-related quote.`,
-    image: {
-      imageUrl: './img/math-magicians.gif',
-      imageAlt: 'preview of Math Magicians project',
-    },
-    technologies: ['html', 'css', 'javaScript', 'React'],
-    liveUrl: 'https://m0rrighan.github.io/math-magicians/',
-    soureUrl: 'https://github.com/M0rrighan/math-magicians',
-  },
-  {
-    id: 3,
-    title: 'Totos - React',
-    details: {
-      client: 'Learning Project',
-      role: 'Full Stack Dev',
-      year: '2022',
-    },
-    shortDescription: `To Do List: allows the user to add tasks, edit and remove tasks,
+		image: {
+			imageUrl: "./img/math-magicians.gif",
+			imageAlt: "preview of Math Magicians project",
+		},
+		technologies: ["html", "css", "javaScript", "React"],
+		liveUrl: "https://calcu-magic.herokuapp.com/",
+		soureUrl: "https://github.com/tmampa/math-magicians",
+	},
+	{
+		id: 3,
+		title: "Totos - React",
+		details: {
+			client: "Learning Project",
+			role: "Full Stack Dev",
+			year: "2022",
+		},
+		shortDescription: `To Do List: allows the user to add tasks, edit and remove tasks,
     clear all completed task`,
-    description: `To Do List: allows the user to add tasks, edit and remove tasks,
+		description: `To Do List: allows the user to add tasks, edit and remove tasks,
     clear all completed task`,
-    image: {
-      imageUrl: './img/todos-react.gif',
-      imageAlt: 'preview of ToDos project',
-    },
-    technologies: ['html', 'css', 'javaScript', 'React'],
-    liveUrl: 'https://m0rrighan.github.io/to-do-react/',
-    soureUrl: 'https://github.com/M0rrighan/to-do-react/tree/react-routing',
-  },
-  {
-    id: 4,
-    title: 'To Do List',
-    details: {
-      client: 'Learning Project',
-      role: 'Full Stack Dev',
-      year: '2021',
-    },
-    shortDescription: `A simple yet powerful to-do list, inspired by the minimalist website.
+		image: {
+			imageUrl: "./img/todos-react.gif",
+			imageAlt: "preview of ToDos project",
+		},
+		technologies: ["html", "css", "javaScript", "React"],
+		liveUrl: "https://m0rrighan.github.io/to-do-react/",
+		soureUrl: "https://github.com/M0rrighan/to-do-react/tree/react-routing",
+	},
+	{
+		id: 4,
+		title: "To Do List",
+		details: {
+			client: "Learning Project",
+			role: "Full Stack Dev",
+			year: "2021",
+		},
+		shortDescription: `A simple yet powerful to-do list, inspired by the minimalist website.
     It allows the user to add tasks, edit and remove tasks, clear all completed tasks.`,
-    description: `A simple yet powerful to-do list, inspired by the minimalist website.
+		description: `A simple yet powerful to-do list, inspired by the minimalist website.
     It allows the user to add tasks, edit and remove tasks, clear all completed tasks.
     Drag and drop is also allowed, giving the possibility to re order the list as pleased.`,
-    image: {
-      imageUrl: './img/TDL.gif',
-      imageAlt: 'preview of To Do List project',
-    },
-    technologies: ['html', 'css', 'javaScript'],
-    liveUrl: 'https://m0rrighan.github.io/To-do-list/',
-    soureUrl: 'https://github.com/M0rrighan/To-do-list',
-  },
-  {
-    id: 5,
-    title: 'LeaderBoard',
-    details: {
-      client: 'Learning Project',
-      role: 'Full Stack Dev',
-      year: '2021',
-    },
-    shortDescription: `The leaderboard website displays scores submitted by you and by other players.
+		image: {
+			imageUrl: "./img/TDL.gif",
+			imageAlt: "preview of To Do List project",
+		},
+		technologies: ["html", "css", "javaScript"],
+		liveUrl: "https://tmampa.github.io/todo-list/dist",
+		soureUrl: "https://github.com/tmampa/todo-list",
+	},
+	{
+		id: 5,
+		title: "LeaderBoard",
+		details: {
+			client: "Learning Project",
+			role: "Full Stack Dev",
+			year: "2021",
+		},
+		shortDescription: `The leaderboard website displays scores submitted by you and by other players.
     Preserves all data to external API servise.`,
-    description: `The leaderboard website displays scores submitted by different players.
+		description: `The leaderboard website displays scores submitted by different players.
     It also allows you to submit your score. All data is preserved thanks to the external
     Leaderboard API service.`,
-    image: {
-      imageUrl: './img/leaderboard.gif',
-      imageAlt: 'preview of Leadwrboard project',
-    },
-    technologies: ['html', 'css', 'javaScript', 'WEBPACK'],
-    liveUrl: 'https://m0rrighan.github.io/Leaderboard/',
-    soureUrl: 'https://github.com/M0rrighan/Leaderboard',
-  },
+		image: {
+			imageUrl: "./img/leaderboard.gif",
+			imageAlt: "preview of Leadwrboard project",
+		},
+		technologies: ["html", "css", "javaScript", "WEBPACK"],
+		liveUrl: "https://github.com/tmampa/Leaderboard",
+		soureUrl: "https://github.com/tmampa/Leaderboard",
+	},
 ];
 
 function createCard(project) {
